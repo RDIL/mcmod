7f0bf225331b8f0ed2a668d2df0de979dd89f4ca
Reece Dunham <me@rdil.rocks>
2019-10-28 16:57:11 +0000

    fixed some bugs

--- src/main/java/rocks/rdil/minecraftmod/ModBootstrap.java
+++ src/main/java/rocks/rdil/minecraftmod/ModBootstrap.java
@@ -1,17 +1,19 @@
 package rocks.rdil.minecraftmod;
 
-import net.minecraft.launchwrapper.ITweaker;
-import net.minecraft.launchwrapper.LaunchClassLoader;
-import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import org.spongepowered.asm.launch.MixinBootstrap;
-import org.spongepowered.asm.mixin.MixinEnvironment;
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.CodeSource;
 import java.util.List;
+
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
+import net.minecraft.launchwrapper.ITweaker;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 @IFMLLoadingPlugin.SortingIndex(1)
 public class ModBootstrap implements ITweaker {
@@ -29,7 +31,6 @@ public class ModBootstrap implements ITweaker {
 
         MixinEnvironment environment = MixinEnvironment.getDefaultEnvironment();
         Mixins.addConfiguration("mixins.MinecraftMod.json");
-        MixinEnvironment.setCompatibilityLevel(MixinEnvironment.CompatibilityLevel.JAVA_8);
         environment.setObfuscationContext("searge");
         environment.setSide(MixinEnvironment.Side.CLIENT);
 
